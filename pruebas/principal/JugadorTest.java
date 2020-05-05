6dcb82cbfad630f4c7fc66bbb42fe7f184dacd86
iker <iker@iker-ZenBook-UX533FD-UX533FD>
2020-05-05 10:59:39 +0200

    Añadida la clase vacia JugadorTest

---
+++ pruebas/principal/JugadorTest.java
@@ -0,0 +1,59 @@
+package principal;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JugadorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetJugador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaquearHabtiacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRuidoEnHabitacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCambiarHabitacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSalirDeCasa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIDHabitacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerderPartida() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHaPerdido() {
+		fail("Not yet implemented");
+	}
+
+}
