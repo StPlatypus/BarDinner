efa6bdd43b2803228776318af74f637773fa8ce5
iker <iker@iker-ZenBook-UX533FD-UX533FD>
2020-05-22 13:25:16 +0200

    Terminado HabitacionTest

--- pruebas/principal/HabitacionTest.java
+++ pruebas/principal/HabitacionTest.java
@@ -8,37 +8,59 @@ import org.junit.Test;
 
 public class HabitacionTest {
 
-	Bannio
+	Banio banio;
+	Cocina cocina;
+	Salon salon;
+	Cuarto cuarto;
+	Jugador jugador;
 	
 	@Before
 	public void setUp() throws Exception {
 		
-		
-		
+		banio = new Banio();
+		cocina = new Cocina();
+		salon = new Salon();
+		cuarto = new Cuarto();
+		jugador = Jugador.getJugador();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
+		banio = null;
+		cocina = null;
+		salon = null;
+		cuarto = null;
+		
 	}
 
 	@Test
 	public void testHabitacion() {
-		fail("Not yet implemented");
+		
+		assertTrue(banio instanceof Banio);
+		assertTrue(cocina instanceof Cocina);
+		assertTrue(salon instanceof Salon);
+		assertTrue(cuarto instanceof Cuarto);
+		
+		
 	}
 
 	@Test
 	public void testAsignarBotinTotal() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetIDHabitacion() {
-		fail("Not yet implemented");
+		
+		assertTrue(banio.saquear()>=50 && banio.saquear()<=200);
+		assertTrue(cocina.saquear()>=80 && cocina.saquear()<=700);
+		assertTrue(salon.saquear()>=500 && salon.saquear()<=2200);
+		assertTrue(cuarto.saquear()>=200 && cuarto.saquear()<=1500);
+		
 	}
 
 	@Test
 	public void testSaquear() {
-		fail("Not yet implemented");
+		banio.saquear();
+		assertSame(banio.saquear(), 0);
+		cocina.saquear();
+		assertSame(cocina.saquear(),0);
 	}
 
 }
