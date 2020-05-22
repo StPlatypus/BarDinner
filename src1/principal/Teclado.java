de3b921eda96ea10597ee8e9a1d00f77be9a8dab
BuildTools <unconfigured@null.spigotmc.org>
2020-05-22 13:39:20 +0200

    Arreglado el teclado. Añadida excepcion cuando se metia un valor no int.

--- src1/principal/Teclado.java
+++ src1/principal/Teclado.java
@@ -1,5 +1,6 @@
 package principal;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Teclado {
@@ -22,10 +23,20 @@ public static Teclado getTeclado()
 	return miTeclado;
 }
 public int leerTeclado()
-{
-	int entradaTeclado;
+{	int entradaTeclado=0;
+boolean chivato=true;
+while (chivato==true){//se hace loop hasta que se meta un valor aceptable
+	try{
 	entradaTeclado = sc.nextInt();
 	System.out.println(entradaTeclado);
+	chivato=false;
+	
+	}catch (InputMismatchException exception) //Esta excepcion salta cuando lo que recoge el scanner no coincide con lo que era en un principio
+	{ 
+	    System.out.println("Solo se pueden usar valores tipo entero."); 
+	    sc.next();//Esto salta el valor no int y pasa al siguiente
+	} }
 	return entradaTeclado;
 }
 }
+	
