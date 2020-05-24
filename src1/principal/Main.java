a492d5ece30a5c524c4324afbbe12b4545d98cfc
BuildTools <unconfigured@null.spigotmc.org>
2020-05-24 10:59:46 +0200

    Eliminados comentarios innecesarios en principal.

--- src1/principal/Main.java
+++ src1/principal/Main.java
@@ -3,8 +3,8 @@ package principal;
 public class Main {
 
 	public static void main(String[] args) {
-		//SUBIDOAA1
-		// TODO Auto-generated method stub
+		
+
 		ClasePrincipal.getClasePrincipal().jugarUnaPartida();
 	}
 
